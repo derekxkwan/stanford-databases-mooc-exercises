c7aed270703f32302b638664a8f0b9db944f1c89
Derek Kwan <derek.x.kwan@gmail.com>
2020-05-07 17:56:05 -0700

    add q7

--- sql/move-rating-query-exercises.sql
+++ sql/move-rating-query-exercises.sql
@@ -27,3 +27,10 @@ order by name, title, stars;
 select name, title
 from (rating r1 join rating r2 using (rID)) join reviewer using (rID) join movie using (mID)
 where r1.mID = r2.mID and r1.ratingDate < r2.ratingDate and r1.stars < r2.stars;
+                                      
+-- q7
+select title, max(stars)
+from movie join rating using (mID)
+group by title
+having count(*) > 1
+order by title;
